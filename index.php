7b31cd4fc12f120df8657027922e40f2a618be3d
Antonio Silva <antoni-s@outlook.com>
2018-11-25 20:48:39 -0200

    Update index.php

--- index.php
+++ index.php
@@ -19,11 +19,11 @@
 
           <div class="centered">
             <h1><?php echo $cotacao->name?></h1>
-            <p>Código: <?php echo $cotacao->code?></p>
-            <p>Código: <?php echo $cotacao->codein?></p>
-            <p>Alto: <?php echo $cotacao->high?></p>
-            <p>Baixo: <?php echo $cotacao->low?></p>
-            <p>Data: <?php echo $cotacao->create_date?></p>
+            <p>Moeda Original: <?php echo $cotacao->code?></p>
+            <p>Convertido para: <?php echo $cotacao->codein?></p>
+            <p>Valor Alto Práticado: <?php echo $cotacao->high?></p>
+            <p>Valor Baixo Práticado: <?php echo $cotacao->low?></p>
+            <p>Última Atualição: <?php echo $cotacao->create_date?></p>
           </div>
 
         </div>
